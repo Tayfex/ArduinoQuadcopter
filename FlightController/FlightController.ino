03482d68b8c33526ce762b968772f74d70126b16
Tayfex <tmfdsm@web.de>
2020-02-09 19:53:27 +0100

    updated servo connections due to new soldering

--- FlightController/FlightController.ino
+++ FlightController/FlightController.ino
@@ -79,10 +79,10 @@ void setup() {
   time_current = millis();
 
   /* Attach the motors */
-  motor_1.attach(8);
-  motor_2.attach(9);
-  motor_3.attach(10);
-  motor_4.attach(11);
+  motor_1.attach(13);                                
+  motor_2.attach(11);
+  motor_3.attach(9);
+  motor_4.attach(8);
 
   Serial.println("Motors attached");
 
